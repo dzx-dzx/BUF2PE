75da43344f2dbc2385221ddb0bf4cbaaa6430a9e
dzx-dzx <dzx_dzx@hotmail.com>
2022-11-02 18:26:59 +0800

    .

---
+++ src/main/scala/PE.scala
@@ -0,0 +1,39 @@
+package BUF2PE
+
+import spinal.core._
+import spinal.lib._
+
+class PE(operandType: HardType[SFix]) extends Component {
+  val io = new Bundle {
+    val weight = in(operandType)
+    val activation = new Bundle {
+      val buffer, fifo, shift_in = in(operandType)
+      val source                 = in(ActivationSource())
+      val shift_out              = out(operandType)
+    }
+    val output = out(operandType)
+  }
+  val mac = new MAC(operandType)
+  switch(io.activation.source) {
+    is(ActivationSource.BUFFER) {
+      mac.io.activation := io.activation.buffer
+    }
+    is(ActivationSource.FIFO) {
+      mac.io.activation := io.activation.fifo
+    }
+    is(ActivationSource.SHIFT) {
+      mac.io.activation := io.activation.shift_in
+    }
+  }
+  mac.io.weight := io.weight
+  io.output     := mac.io.output
+
+  io.activation.shift_out := RegNext(io.activation.shift_in)
+}
+object PE {
+  def main(args: Array[String]): Unit = {
+    SpinalConfig(targetDirectory = "rtl").generateVerilog(
+      gen = new PE(SFix(peak = 8 exp, width = 16 bits))
+    )
+  }
+}
--- src/main/scala/Pox.scala
+++ src/main/scala/Pox.scala
@@ -2,31 +2,35 @@ package BUF2PE
 
 import spinal.core._
 import spinal.lib._
-class Pox(operandType: HardType[SFix]) extends Component {
+
+class Pox(operandType: HardType[SFix], pox: Int = 3, kernel_size: Int = 3) extends Component {
   val io = new Bundle {
     val weight = in(operandType)
     val activation = new Bundle {
-      val buffer, fifo, shift_in = in(operandType)
-      val source                 = in(ActivationSource())
+      val buffer, fifo_in = in(Vec(operandType, pox))
+      val buffer_standby  = in(operandType)
+      val source          = in(ActivationSource())
+      val fifo_out        = out(Vec(operandType, pox))
     }
-    val output, shift_out = out(operandType)
+    val output = out(Vec(operandType, pox))
   }
-  val mac = new MAC(operandType)
-  switch(io.activation.source) {
-    is(ActivationSource.BUFFER) {
-      mac.io.activation := io.activation.buffer
-    }
-    is(ActivationSource.FIFO) {
-      mac.io.activation := io.activation.fifo
-    }
-    is(ActivationSource.SHIFT) {
-      mac.io.activation := io.activation.shift_in
-    }
+  val pe_array: Seq[PE] = Seq.fill(pox)(new PE(operandType))
+  for (i <- 0 until pox) {
+    pe_array(i).io.weight := io.weight
+
+    pe_array(i).io.activation.source := io.activation.source
+    pe_array(i).io.activation.buffer := io.activation.buffer(i)
+    pe_array(i).io.activation.fifo   := io.activation.fifo_in(i)
+
+    if (i == pox - 1) {
+      pe_array(i).io.activation.shift_in := io.activation.buffer_standby
+    } else { pe_array(i).io.activation.shift_in := pe_array(i + 1).io.activation.shift_out }
+
+    io.activation.fifo_out(i) := History(pe_array(i).io.activation.shift_out, kernel_size).last
+    io.output(i)              := pe_array(i).io.output
   }
-  mac.io.weight := io.weight
-  io.output     := mac.io.output
-  io.shift_out  := RegNext(io.activation.shift_in)
 }
+
 object Pox {
   def main(args: Array[String]): Unit = {
     SpinalConfig(targetDirectory = "rtl").generateVerilog(
