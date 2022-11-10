7eb0e1a2202e99c5a75508e1db46a1f357d7f68d
dzx-dzx <dzx_dzx@hotmail.com>
2022-11-11 02:11:20 +0800

    Use better testcase.

--- src/test/scala/PoyTestbench.scala
+++ src/test/scala/PoyTestbench.scala
@@ -7,6 +7,7 @@ import spinal.lib._
 import scala.collection.mutable.Queue
 import org.scalatest.BeforeAndAfter
 import scala.util.Random
+import scala.math._
 
 class PoyTestbench extends AnyFunSuite with BeforeAndAfter {
   val poy         = 3
@@ -30,28 +31,28 @@ class PoyTestbench extends AnyFunSuite with BeforeAndAfter {
         // val activation_full = Seq.fill(poy + kernel_size - 1)(Seq.fill(pox + kernel_size - 1)(Random.nextInt()))
 
         clockDomain.forkStimulus(5)
-        io.clear #= true
+        io.clear     #= true
         io.reset_mac #= true
 
         clockDomain.waitSampling(10)
-        io.clear #= false
+        io.clear     #= false
         io.reset_mac #= false
-        io.weight #= 0.01
+        io.weight    #= pow(2, -5)
         io.activation.buffer.zipWithIndex.foreach { case (v, i) =>
-          v.zipWithIndex.foreach { case (s, j) => s #= i * 0.5 + j * 0.05 }
+          v.zipWithIndex.foreach { case (s, j) => s #= i * pow(2, -5) + j * pow(2, -7) }
         }
-        io.activation.buffer_standby.zipWithIndex.foreach { case (s, i) => s #= i * 0.05 }
+        io.activation.buffer_standby.zipWithIndex.foreach { case (s, i) => s #= i * pow(2, -6) }
 
         print(SFix(peak = 2 exp, width = 16 bits).maxValue)
 
         for (i <- 0 until 18) {
           clockDomain.waitSampling()
           fork {
-            io.activation.buffer.foreach(_.foreach(s => s #= s.toBigDecimal + 0.05))
-            io.activation.buffer_standby.foreach(s => s #= s.toBigDecimal + 0.05)
+            io.activation.buffer.foreach(_.foreach(s => s #= s.toBigDecimal + pow(2, -10)))
+            io.activation.buffer_standby.foreach(s => s #= s.toBigDecimal + pow(2, -10))
           }
           fork {
-            io.weight.randomize()
+            io.weight #= io.weight.toBigDecimal + pow(2, -5)
           }
         }
 
